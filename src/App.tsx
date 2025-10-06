34e7ab7ad0e5b2e7b5595210dbf876154afd791e
aksundal <143636555+aksundal@users.noreply.github.com>
2025-10-06 23:51:25 +0200

    simple responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import heroImage from "./assets/Profilbilde CV 1.jpg";
 import { Heading, Tabs, Box, List, Accordion, Link } from '@navikt/ds-react'
-import { ClockIcon } from '@navikt/aksel-icons';
+import { ClockIcon, BookIcon, HatSchoolIcon, RulersIcon } from '@navikt/aksel-icons';
+import GitHubLogo from "./assets/github-logo.svg";
+import LinkedInLogo from "./assets/linkedin-logo.svg"
 
 function App() {
 
@@ -16,7 +18,7 @@ function App() {
         </div>
       </nav>
       <main id="main">
-      <Heading size="medium" className="subHeader"> Om Meg! </Heading>
+      <Heading size="large" className="sub-header"> Om Meg! </Heading>
       <section id="hero">
         <div id="hero-text">
           <Heading size="small">Vyrde rekrutteringsteamet i NAV,</Heading>
@@ -25,22 +27,26 @@ function App() {
 og blir ferdig våren 2026. Då eg såg utlysinga dykkar å kjende eg meg umiddelbart igjen i beskrivinga av kva dykk ser etter: 
 ein nysgjerrig, samarbeidsorientert og engasjert utviklar som vil bidra til noko som faktisk betyr noko.
           </p>
+          <div id="contact-container">
+            <Link href="https://github.com/aksundal/"><img src={GitHubLogo} width="24" height="24"/></Link>
+            <Link href="www.linkedin.com/in/anne-kristin-villar-sundal"><img src={LinkedInLogo} width="24" height="24"/></Link>
+          </div>
         </div>
         <div id="hero-img" >
           <img src={heroImage}/>
         </div>
       </section>
-      <Heading size="medium" className="ssub-header"> Min Erfaring! </Heading>
+      <Heading size="large" className="sub-header"> Min Erfaring! </Heading>
       <section id="experience">
         <Tabs defaultValue="Erfaring">
           <Tabs.List>
             <Tabs.Tab value="Erfaring" label="Erfaring" icon={<ClockIcon aria-hidden/>}/>
-            <Tabs.Tab value="Ferdigheter" label="Ferdigheter" icon={<ClockIcon aria-hidden/>}/>
-            <Tabs.Tab value="Prosjekter" label="Prosjekter" icon={<ClockIcon aria-hidden/>}/>
-            <Tabs.Tab value="Utdanning" label="Utdanning" icon={<ClockIcon aria-hidden/>}/>
+            <Tabs.Tab value="Ferdigheter" label="Ferdigheter" icon={<RulersIcon aria-hidden/>}/>
+            <Tabs.Tab value="Prosjekter" label="Prosjekter" icon={<BookIcon aria-hidden/>}/>
+            <Tabs.Tab value="Utdanning" label="Utdanning" icon={<HatSchoolIcon aria-hidden/>}/>
           </Tabs.List>
           <Tabs.Panel value="Erfaring">
-            <Box width="100%" height="20rem" padding="space-16">
+            <Box className='tabs-box'>
             <List as="ul" className="info-liste">
                 <List.Item>
                 <b>Fullstack- og frontendprosjekt - </b>
@@ -62,67 +68,80 @@ ein nysgjerrig, samarbeidsorientert og engasjert utviklar som vil bidra til noko
             
           </Tabs.Panel>
           <Tabs.Panel value="Ferdigheter">
-            <Box width="100%" height="20rem" padding="space-16">
+            <Box className='tabs-box'>
               <List as="ul" className="info-liste">
                 <List.Item>
-                Tekniske: Java, Kotlin, Swift, Javascript/Typescript, React, React Native, C#, SQL, Unity
+                <b>Tekniske ferdigheter: </b>
+                Java, Kotlin, Swift, Javascript/Typescript, React, React Native, C#, SQL, Unity, Figma, Git/Github, REST API, .NET, HTML5, CSS3, .NET
                 </List.Item>
                 <List.Item>
-                  har full eller gradert uføretrygd, men ønsker å jobbe. Nav må ha vurdert om
-                  andre arbeidsmarkedstiltak og virkemidler er aktuelle, eller
-                </List.Item>
-                <List.Item>
-                  er en arbeidssøker med varig og vesentlig nedsatt arbeidsevne som kan bli
-                  ansatt i en vanlig jobb.
+                  <b>Sosiale ferdigheter: </b>
+                  God samarbeidsevne, sosial sommerfugl, omsorgsfull, elske å bli kjent med folk, målretta, sterkt arbeidsfokus.
                 </List.Item>
               </List>
             </Box>
           </Tabs.Panel>
           <Tabs.Panel value="Prosjekter">
-            <Box width="100%" height="20rem" padding="space-16">
+            <Box className='tabs-box'>
               <List as="ul" className="info-liste">
-                <List.Item>
-                  står i fare for å miste jobben etter å ha vært sykmeldt helt eller delvis i
-                  12 måneder,
+              <List.Item>
+                <b>Dreams of Skyrim - Kreativt Webprosjekt Haust 2023/Høyskolen Kristiania, Oslo -  </b>
+                Utvikle frontend-løysning med bruk av CSS3 og HTML5, fokus på strukturert kode og responsivt design. 
+                <Link href="https://github.com/aksundal/Dreams-of-skyrim">Github</Link>
                 </List.Item>
                 <List.Item>
-                  har full eller gradert uføretrygd, men ønsker å jobbe. Nav må ha vurdert om
-                  andre arbeidsmarkedstiltak og virkemidler er aktuelle, eller
+                <b>PawPal - Interaksjonsdesign Haust 2024/ Høyskolen Kristiania, Oslo -  </b>
+                Innsikt i interaksjonsprosessen og utarbeid ein interaktiv High- Fidelity prototype i Figma. 
+                Utforme ein applikasjon som oppfyller eit brukerbehov: brukere som eige hunder og leiter etter eit nettverk for å sosialisere seg sjølv og hunden sin.
+                Sikre at applikasjonen, PawPal, oppfyller krava til universell utforming.
+                <Link href="https://www.figma.com/proto/AOlRiELkcT76itOTuEGeoc/Pawpal?node-id=229-198&p=f&t=goGHe3lcU5UvYfup-1&scaling=scale-down&content-scaling=fixed&page-id=129%3A914&starting-point-node-id=229%3A198">Github</Link>
                 </List.Item>
                 <List.Item>
-                  er en arbeidssøker med varig og vesentlig nedsatt arbeidsevne som kan bli
-                  ansatt i en vanlig jobb.
+                <b>Python Programming Vår 2025/ Høyskolen Kristiania, Oslo -  </b>
+                Arbeide med grunnleggande og noko vidaregåande Python teknikker.
+                Fokus på datastruktur og logisk kode som oppfyller oppgåvekrava.
+                <Link href="https://github.com/aksundal/PGR107---Python-Programming">Github</Link>
+                </List.Item>
+                <List.Item>
+                <b>Objektorientert orientert programmering Vår 2025/ Høyskolen Kristiania, Oslo - </b>
+                Backend-løysning med Java og MySQL.
+                Utvikle applikasjon basert på OOP konseptene: Objekter, arv, polymorfi, inkapsling og abstraksjon.
+                Lage ein applikasjon som fokusere på god kodestruktur, gjenbrukbar kode og god databehandling.
+                <Link href="https://github.com/aksundal/PGR112-konteksamenV25">Github</Link>
+                </List.Item>
+                <List.Item>
+                <b>Kryssplattform Haust 2025/ Høyskolen Kristiania, Oslo -  </b>
+                Utvikle mobilaplikasjoner med rammeverka React Native og Expo. Fokus på felles kodebase som funke på tvers av plattformer.
+                <Link href="https://github.com/aksundal/Kryssplattform-HK-H25">Github</Link>
+                </List.Item>
+                <List.Item>
+                <b>IOS Programming Haust 2025/ Høyskolen Kristiania, Oslo -  </b>
+                Programmering i Swift. Utvikling av iOS-applikasjoner med UI rammeverk SwiftUI og UIKit.
+                <Link href="https://github.com/aksundal/PG5602">Github</Link>
                 </List.Item>
               </List>
             </Box>
           </Tabs.Panel>
           <Tabs.Panel value="Utdanning">
-            <Box width="100%" height="20rem" padding="space-16">
+            <Box className='tabs-box'>
               <List as="ul" className="info-liste">
                 <List.Item>
-                  står i fare for å miste jobben etter å ha vært sykmeldt helt eller delvis i
-                  12 måneder,
-                </List.Item>
-                <List.Item>
-                  har full eller gradert uføretrygd, men ønsker å jobbe. Nav må ha vurdert om
-                  andre arbeidsmarkedstiltak og virkemidler er aktuelle, eller
-                </List.Item>
-                <List.Item>
-                  er en arbeidssøker med varig og vesentlig nedsatt arbeidsevne som kan bli
-                  ansatt i en vanlig jobb.
+                  <b>Høyskole Kristiania Bachelorgrad Informasjonsteknologi, August 2023 - Juni 2026 - </b>
+                  Spesialisering: Frontend- og mobilutvikling
                 </List.Item>
               </List>
             </Box>
           </Tabs.Panel>
         </Tabs>
       </section>
-      <Heading size='medium' className="sub-header"> Mine Hobbier! </Heading>
+      <Heading size='large' className="sub-header"> Mine Hobbier! </Heading>
       <section id="hobbier">
         <Accordion>
           <Accordion.Item>
             <Accordion.Header>Innholdsproduksjon</Accordion.Header>
             <Accordion.Content>
-              Elske å formidle historier gjennom foto, video og lyd. 
+              Elske å formidle historier gjennom foto, video og lyd.
+              Like å utforske korleis kreativitet kan kombinerast med teknologi.
             </Accordion.Content>
           </Accordion.Item>
           <Accordion.Item>
@@ -130,16 +149,22 @@ ein nysgjerrig, samarbeidsorientert og engasjert utviklar som vil bidra til noko
               Matlaging
             </Accordion.Header>
             <Accordion.Content>
-              Info om hobby 2
+            Får energi av å lage god mat frå botnen av og eksperimentere med nye smakar. 
+            Har gjennom vervet mitt lært mange nye matlagingstekniker.
             </Accordion.Content>
           </Accordion.Item>
           <Accordion.Item>
             <Accordion.Header>Strikking</Accordion.Header>
-            <Accordion.Content>Info om hobby 3</Accordion.Content>
+            <Accordion.Content>Strikking gir meg ro og fokus. 
+              Det handlar om tålmodigheit, mønster og å sjå eit prosjekt vekse fram frå maske til maske.</Accordion.Content>
           </Accordion.Item>
         </Accordion>
       </section>
     </main>
+    <footer>
+      <Heading size="large">Ta Kontakt!</Heading>
+      <p>Dykk når meg på <span><Link href="www.linkedin.com/in/anne-kristin-villar-sundal">Linked In</Link></span> og <Link href="https://github.com/aksundal/">GitHub</Link>.</p>
+    </footer>
     </>
   )
 }
